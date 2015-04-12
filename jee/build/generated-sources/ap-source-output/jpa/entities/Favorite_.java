7510995bfdf2f402e78cf450ee276236bf159fd9
daniel.decarval <daniel.decarvalhofernandes@he-arc.ch>
2015-04-12 15:09:10 +0200

    Ajout de background-image au projet

--- jee/build/generated-sources/ap-source-output/jpa/entities/Favorite_.java
+++ jee/build/generated-sources/ap-source-output/jpa/entities/Favorite_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import jpa.entities.Movie;
 import jpa.entities.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-12T13:35:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-12T14:48:55")
 @StaticMetamodel(Favorite.class)
 public class Favorite_ { 
 
--- jee/build/generated-sources/ap-source-output/jpa/entities/User_.java
+++ jee/build/generated-sources/ap-source-output/jpa/entities/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import jpa.entities.Favorite;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-12T13:35:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-12T14:48:55")
 @StaticMetamodel(User.class)
 public class User_ { 
 
