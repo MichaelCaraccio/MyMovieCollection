d44dad81ce3dc4e706942431a83e19bd751c755c
danieldecarval <daniel.decarvalhofernandes@he-arc.ch>
2015-04-19 17:15:52 +0200

    Finition contrainte administrateur

--- jee/src/java/beans/LoginBean.java
+++ jee/src/java/beans/LoginBean.java
@@ -88,6 +88,7 @@ public class LoginBean {
   
   public String login () throws IOException {
     String message = "";
+    String navto = "/login/Login.xhtml";
         HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
          List queryUser = new ArrayList();
             List queryGroup = new ArrayList();
@@ -104,11 +105,10 @@ public class LoginBean {
             //Display a message based on the User role
             if(request.isUserInRole("Administrator")){
                 message = "Username : " + principal.getName() + " You are an Administrator, you can really f**k things up now";
+                navto = "/admin/AdminDashboard.xhtml";
             }else if(request.isUserInRole("Manager")){
-                 System.out.println("YOLO SWAG 332");
                 message = "Username : " + principal.getName() + " You are only a Manager, Don't you have a Spreadsheet to be working on??";
             }else if(request.isUserInRole("User")){
-                 System.out.println("YOLO SWAG 22");
                 message = "Username : " + principal.getName() + " You're wasting my resources...";
             }
              
@@ -118,14 +118,14 @@ public class LoginBean {
             
             
             
-            return "/login/Login.xhtml";
+            return navto;
             
         } catch (ServletException e) {
             setIsLogged(false);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An Error Occured: Login failed", null));
             e.printStackTrace();
         }
-        return "/login/Login.xhtml";
+        return navto;
   }
 
   public String logout() {
